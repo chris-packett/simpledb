a318f0b3dd8a4cb3e65a7844a9ae0ad4565892b5
pacman <chris@relativitytech.com>
2022-09-25 22:45:28 -0400

    cleanup + refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io::Write;
 
-use clap::{Command, Arg};
+use clap::{Command, Arg, ArgMatches};
 
 fn main() -> Result<(), String> {
     loop {
@@ -10,15 +10,14 @@ fn main() -> Result<(), String> {
             continue;
         }
 
-        match respond(line) {
+        match process_command(line) {
             Ok(should_quit) => {
                 if should_quit {
                     break;
                 }
             }
             Err(err) => {
-                write!(std::io::stdout(), "{}", err).map_err(|e| e.to_string())?;
-                std::io::stdout().flush().map_err(|e| e.to_string())?;
+                log_err(err)?;
             }
         }
     }
@@ -26,25 +25,26 @@ fn main() -> Result<(), String> {
     Ok(())
 }
 
-fn respond(line: &str) -> Result<bool, String> {
+fn process_command(line: &str) -> Result<bool, String> {
     let args = shlex::split(line).ok_or("error: invalid quoting")?;
     let matches = cli()
         .try_get_matches_from(&args)
         .map_err(|e| e.to_string())?;
+
+    let should_quit = handle_command(matches)?;
+
+    Ok(should_quit)
+}
+
+fn handle_command(matches: ArgMatches) -> Result<bool, String> {
     match matches.subcommand() {
         Some(("select", sub_matches)) => {
-            let ext_args: Vec<&str> = match sub_matches.get_many::<String>("ext") {
-                Some(iter) => iter.map(|s| s.as_str()).collect(),
-                None => vec![]
-            };
+            let ext_args = parse_ext_args(sub_matches, None);
 
             println!("{:?}", ext_args);
         },
         Some(("insert", sub_matches)) => {
-            let ext_args: Vec<&str> = match sub_matches.get_many::<String>("ext") {
-                Some(iter) => iter.map(|s| s.as_str()).collect(),
-                None => vec![]
-            };
+            let ext_args = parse_ext_args(sub_matches, None);
 
             println!("{:?}", ext_args);
         },
@@ -100,4 +100,23 @@ fn read_line() -> Result<String, String> {
         .read_line(&mut buffer)
         .map_err(|e| e.to_string())?;
     Ok(buffer)
+}
+
+fn log_err(err: String) -> Result<(), String> {
+    write!(std::io::stdout(), "{}", err).map_err(|e| e.to_string())?;
+    std::io::stdout().flush().map_err(|e| e.to_string())?;
+
+    Ok(())
+}
+
+/// Parses the extra (trailing) arguments from a subcommand.
+fn parse_ext_args(sub_matches: &ArgMatches, ext_args_name: Option<String>) -> Vec<&str> {
+    const DEFAULT_EXT_ARGS_NAME: &str = "ext";
+    let ext_args_name = ext_args_name.unwrap_or_else(|| String::from(DEFAULT_EXT_ARGS_NAME));
+    let ext_args: Vec<&str> = match sub_matches.get_many::<String>(&ext_args_name) {
+        Some(iter) => iter.map(|s| s.as_str()).collect(),
+        None => vec![]
+    };
+
+    ext_args
 }
